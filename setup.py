c02d448552ea26f6e5156b5a65ba0488940d8933
Eitan Isaacson <eitan@ascender.com>
2009-10-21 09:54:37 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import interwibble
+
+setup(
+    name         = 'interwibble',
+    version      = interwibble.VERSION,
+    description  = interwibble.__doc__,
+    author       = 'Eitan Isaacson',
+    author_email = 'eitan@monotonous.org',
+    url          = 'http://monotonous.org/',
+    license      = 'GPL',
+    classifiers  = ['Development Status :: 3 - Alpha',
+                    'Environment :: Console',
+                    'Intended Audience :: End Users/Desktop',
+                    'Intended Audience :: Information Technology',
+                    'License :: OSI Approved :: GNU General Public License (GPL)',
+                    'Operating System :: POSIX',
+                    'Programming Language :: Python',
+                    'Topic :: Internet :: WWW/HTTP',
+                    'Topic :: Text Processing :: Markup :: HTML'
+                    ],
+    scripts      = ['interwibble']
+    )
